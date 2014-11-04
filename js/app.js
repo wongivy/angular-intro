40bef7e3d7d6411dd6b2942f9305a579773b3b9f
Ivy <wongivy13@yahoo.com>
2014-11-04 12:14:28 -0800

    End of class commit

--- js/app.js
+++ js/app.js
@@ -5,3 +5,25 @@
 
 "use strict";
 
+angular.module('AngularDemo', [])
+    .controller('HelloController', function($scope) {
+        $scope.person = {
+           firstName: null,
+           lastName: null
+       } ;
+    })
+    .controller('MoviesController', function($scope) {
+        $scope.movies = movies;
+        $scope.sortCol = 'rank';
+        $scope.sortBy = function(sortCol) {
+            if ($scope.sortCol == sortCol) {
+                $scope.sortReverse = !$scope.sortReverse;
+            } else {
+                $scope.sortCol = sortCol;
+                $scope.sortReverse = false;
+            }
+        };
+        $scope.isSortedBy = function(colName) {
+            return colName == $scope.sortCol;
+        }
+    });
